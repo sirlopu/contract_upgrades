817f728c580b50b0fb85dbedbe8cf9be41be0193
Gabriel S <sirlopug@gmail.com>
2022-03-27 01:48:28 -0700

    initial commit

---
+++ scripts/helpful_scripts.py
@@ -0,0 +1,44 @@
+from brownie import accounts, network, config
+import eth_utils
+
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork-dev"]
+NON_FORKED_LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["hardhat", "development", "ganache"]
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = NON_FORKED_LOCAL_BLOCKCHAIN_ENVIRONMENTS + [
+    "mainnet-fork",
+    "binance-fork",
+    "matic-fork",
+    "ganache-local",
+]
+
+
+def get_account(index=None, id=None):
+    if index:
+        return accounts[index]
+    if id:
+        return accounts.load(id)
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+    ):
+        return accounts[0]
+    return accounts.add(config["wallets"]["from_key"])
+
+
+# initializer=box.store, 1
+def encode_function_data(initializer=None, *args):
+    """Encode the function call so we can work with an initializer.
+
+    Args:
+        initializer ([brownie.network.contract.ContractTx], optional):
+        The initializer function we want to call. Example: `box.store`.
+        Default to None.
+
+        args (Any, optional):
+        The arguments to pass to the initializer function
+
+    Returns:
+        [bytes]: Return the encoded bytes.
+    """
+    if len(args) == 0 or not initializer:
+        return eth_utils.to_bytes(hexstr="0x")
+    return initializer.encode_input(*args)
